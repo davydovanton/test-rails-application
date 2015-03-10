f159a5530de1605c8595ea79ea2423064107e6e6
Anton Davydov <antondavydov.o@gmail.com>
2015-03-10 20:52:04 +0300

    Add redis hach

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,11 +2,17 @@ run_sidekiq_in_this_thread = %w(staging).include?(ENV['RAILS_ENV'])
 worker_processes (run_sidekiq_in_this_thread ? 2 : 3)
 # whatever you had in your unicorn.rb file
 @sidekiq_pid = nil
+timeout 15 
 
 before_fork do |server, worker|
   # ... whatever you had in your before_fork ...
   if run_sidekiq_in_this_thread
     @resque_pid ||= spawn("bundle exec sidekiq -c 2")
     Rails.logger.info('Spawned sidekiq #{@request_pid}')
+  end
+
+  if defined?(Resque)
+    Resque.redis.quit
+    Rails.logger.info('Disconnected from Redis')
   end
 end
